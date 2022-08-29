b0718dcd5018ec771a402ce3b2d5c36d66b7da7b
Nikolay <brunyanp@gmail.com>
2022-08-29 16:37:09 +0700

    final task 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,20 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
 
-    AutoFactory autoFactory = new AutoFactory();
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+       public static void main( String[] args )    {
+
+           ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+           AutoFactory autoFactory = ctx.getBean("autoFactory", AutoFactory.class);
+           AutoFactory autoFactory1 = ctx.getBean("autoFactory1", AutoFactory.class);
+           autoFactory.run();
+           autoFactory1.run();
+           System.out.println("Num cars:" + autoFactory.getNumCars());
+           System.out.println("Num cars:" + autoFactory1.getNumCars());
     }
 
 
