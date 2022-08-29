af296b566c8c6d4b0ab65c6a01674bc2b94c3ee7
Nikolay <brunyanp@gmail.com>
2022-08-29 14:36:49 +0700

    Test commit 2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,12 @@ package org.example;
  */
 public class App 
 {
+
+    AutoFactory autoFactory = new AutoFactory();
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+
+
 }
